a1a41422246bc9dd15484f43015b4eb8f98f0144
Karl Stolley <karl.stolley@gmail.com>
2018-06-23 16:35:42 -0500

    Simplify wind-direction function

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,7 @@ module.exports = router;
 
 // Output wind direction from degrees
 function wind_direction(degrees) {
-  let index;
   const d = ['N','NNE','NE','ENE','E','ESE','SE','SSE','S','SSW','SW','WSW','W','WNW','NW','NNW','N'];
-  index = Math.round(degrees/22.5);
+  const index = Math.round(degrees/22.5);
   return d[index];
 }
