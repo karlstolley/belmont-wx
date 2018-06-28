04dbd90e14abca905bc80b09111334829e96f136
Karl Stolley <karl.stolley@gmail.com>
2018-06-28 09:00:31 -0500

    Start building out route and diagnostic view

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,25 @@
 const express = require('express');
 const router = express.Router();
 
+const fs = require('fs');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  fs.readFile('_data/rest_data.json', (err, data) => {
+    if (err) throw err;
+    let weather = JSON.parse(data);
+    const trends = {
+      temps: [],
+      barom: []
+    };
+    for (let obs of weather) {
+      trends.temps.push(obs.tempf);
+      trends.barom.push(obs.baromrelin);
+    }
+    trends.temps = trends.temps.join(',');
+    trends.barom = trends.barom.join(',');
+    res.render('index', { title: 'Current conditions', current_obs: weather[0], trends: trends });
+  });
 });
 
 module.exports = router;
